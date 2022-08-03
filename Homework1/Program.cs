5b2c8fe67f406beee80f1b6100819339204e1b9b
rovsencode <rovshanakh@code.edu.az>
2022-08-03 13:39:47 +0400

    task2

---
+++ Homework1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "acdemy";
+            bool founder = false;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == 'a')
+                {
+                    founder = true;
+                }
+               
+            }
+            if (founder==true)
+            {
+                Console.WriteLine("a letter is into word");
+            }
+            else
+            {
+                Console.WriteLine("a letter is not into word");
+            }
+        }
+    }
+}
